5e866066422ab2d0a17e457616181def0ed85241
yilin xu <xu.yilin@husky.neu.edu>
2020-07-30 10:21:34 -0400

    add independent state of likes and now I can refresh likes immediately

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -4,6 +4,7 @@ import blogService from "../services/blogs";
 const Blog = ({ blog }) => {
   const [show, setShow] = useState(false);
   const [blogEntry, setBlogEntry] = useState(blog)
+  const [likes, setLikes] = useState(blog.likes)
   const toggleShow = () => {
     setShow(!show);
   };
@@ -11,14 +12,15 @@ const Blog = ({ blog }) => {
   const hideWhenShow = { display: show ? "none" : "" };
   const renderWhenShow = { display: show ? "" : "none" };
 
-  const likes = async ()=>{
+  const handleLike = async ()=>{
     try{
       blog.likes = blogEntry.likes
       blog.likes = blog.likes+1
       await blogService.update(blog)
       setBlogEntry(blog)
-      console.log("updated blog entry")
-      console.log(blogEntry)
+      setLikes(blog.likes)
+      // console.log("updated blog entry")
+      // console.log(blogEntry)
     }
     catch (exception){
       console.log(exception)
@@ -37,7 +39,7 @@ const Blog = ({ blog }) => {
           {blogEntry.title} <button onClick={toggleShow}>hide</button>
         </p>
         <p className="line">{blogEntry.url}</p>
-        <p className="line">{blogEntry.likes} <button onClick={likes}>like</button></p>
+        <p className="line">{likes} <button onClick={handleLike}>like</button></p>
         <p className="line">{blogEntry.user.name}</p>
       </div>
     </div>
