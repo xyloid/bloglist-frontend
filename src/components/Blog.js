04e81db00e368ffdc8dc4af6e9291bd5caa07482
yilin xu <xu.yilin@husky.neu.edu>
2020-07-30 09:41:00 -0400

    hide/view blog entry

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,29 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const [show, setShow] = useState(false);
+  const toggleShow = () => {
+    setShow(!show);
+  };
 
-export default Blog
+  const hideWhenShow = { display: show ? "none" : "" };
+  const renderWhenShow = { display: show ? "" : "none" };
+
+  return (
+    <div>
+      <div className="blog" style={hideWhenShow}>
+        {blog.title}
+        <button onClick={toggleShow}>view</button>
+      </div>
+      <div className="blog" style={renderWhenShow}>
+        <p className="line">
+          {blog.title} <button onClick={toggleShow}>hide</button>
+        </p>
+        <p className="line">{blog.url}</p>
+        <p className="line">{blog.likes}</p>
+        <p className="line">{blog.user.name}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
