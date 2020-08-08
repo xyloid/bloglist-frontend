bd7d9f42f62a233d26f7a72cfaccda44efc54992
yilin xu <xu.yilin@husky.neu.edu>
2020-08-08 15:38:06 -0400

    add dummy content

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,6 +1,13 @@
 
 const UserDetails = ()=>{
-
+return (<div>
+    <h1>User Name</h1>
+    <h2>added blogs</h2>
+    <ul>
+        <li>1st</li>
+        <li>2nd</li>
+    </ul>
+</div>)
 }
 
 export default UserDetails
