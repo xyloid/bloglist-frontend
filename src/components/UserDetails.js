43ebe4848549a4e23af8534b910aafdc2f5ed41e
yilin xu <xu.yilin@husky.neu.edu>
2020-08-08 16:09:53 -0400

    refresh on /users/:id page will get undefined users

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,16 +1,20 @@
 import React from "react";
 const UserDetails = ({ user }) => {
-    console.log(user)
-  return (
-    <div>
-      <h1>name</h1>
-      <h2>added blogs</h2>
-      <ul>
-        <li>1st</li>
-        <li>2nd</li>
-      </ul>
-    </div>
-  );
+  console.log("user details", user);
+  if (user) {
+    return (
+      <div>
+        <h1>{user.name}</h1>
+        <h2>added blogs</h2>
+        <ul>
+          <li>1st</li>
+          <li>2nd</li>
+        </ul>
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default UserDetails;
